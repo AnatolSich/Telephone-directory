9cbc4090e130498fc6250761dab827416488734c
Anatolii Sichkar <anatolii_sichkar@epam.com>
2020-11-21 17:53:01 +0200

    first commit

---
+++ src/main/java/com/springAdvanced/Telephonedirectory/TelephoneDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.springAdvanced.Telephonedirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelephoneDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelephoneDirectoryApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS phone_numbers;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS phone_companies;
+
+DROP SEQUENCE IF EXISTS seq_books;
+DROP SEQUENCE IF EXISTS seq_students;
+
+CREATE SEQUENCE seq_students START 1000;
+CREATE SEQUENCE seq_books START 5000;
+
+CREATE TABLE students (
+  id INT PRIMARY KEY DEFAULT nextval('seq_students'),
+  name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE books(
+  id INT PRIMARY KEY  DEFAULT nextval('seq_books'),
+  title VARCHAR(50) NOT NULL ,
+  studentId INT NOT NULL ,
+  FOREIGN KEY (studentId) REFERENCES students (id) ON DELETE CASCADE 
+);
+
